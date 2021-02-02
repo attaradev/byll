6405cfa6cc5276f658022008085f10dd1154d4a1
Mike Perry Y Attara <mpyebattara@gmail.com>
2021-02-02 17:21:19 +0000

    add resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :timesheets
+  namespace :api do
+    namespace :v1, default: {format: :json} do
+      resources :timesheets, only: [:index, :create, :update, :destroy]
+      resource :invoice, only: [:create]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
