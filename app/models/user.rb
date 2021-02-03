aa8f31a6452f44f495224071bbfc2c6cf678ad28
Mike Perry Y Attara <mpyebattara@gmail.com>
2021-02-03 14:20:27 +0000

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable, :validatable
   enum role: %i[finance_team lawyer], _default: 'lawyer'
+  validates :role, presence: true
 
   def generate_jwt
     JWT.encode({ id: id,
