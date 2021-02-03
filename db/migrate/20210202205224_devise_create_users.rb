5bdb3d4988feaa09534f916d3e024f69babf015b
Mike Perry Y Attara <mpyebattara@gmail.com>
2021-02-03 13:11:30 +0000

    default role migration

--- db/migrate/20210202205224_devise_create_users.rb
+++ db/migrate/20210202205224_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.integer :role,              null: false
+      t.integer :role,              null: false, default: 1
 
       ## Recoverable
       # t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_205224) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "role", null: false
+    t.integer "role", default: 1, null: false
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
